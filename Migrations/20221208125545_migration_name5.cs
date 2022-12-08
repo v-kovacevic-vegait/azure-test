34f27226581286efd437d4f00d312de68ed0aa01
v-kovacevic-vegait <v.kovacevic@vegait.rs>
2022-12-08 13:55:55 +0100

    migrations test

---
+++ Migrations/20221208125545_migration_name5.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MealForFamily.Migrations
+{
+    public partial class migration_name5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NewTest2",
+                table: "AboutUs",
+                newName: "NewTest3");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NewTest3",
+                table: "AboutUs",
+                newName: "NewTest2");
+        }
+    }
+}
